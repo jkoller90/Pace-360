8387eb9a09059cf7eaf7098f6b3b4a53af76b1c8
HarshitaKudaravalli <reach.harshitha@gmail.com>
2017-12-18 11:07:16 -0500

    scenes added

---
+++ Pace360/Assets/scripts/back.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class back : MonoBehaviour
+{
+
+    private bool gazedAt = false;
+    private float gazedTime = 2.0f;
+    private float timer = 0.0f;
+    private Slider slider = null;
+
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gazedAt)
+        {
+            timer += Time.deltaTime;
+
+            if (slider.value >= 1.0f)
+            {
+                SceneManager.LoadScene("Home");
+                timer = 0.0f;
+            }
+        }
+        else
+        {
+            timer = 0.0f;
+        }
+        slider.value = timer / gazedTime;
+    }
+
+    public void PointerEnter()
+    {
+        gazedAt = true;
+
+    }
+
+    public void PointerExit()
+    {
+        gazedAt = false;
+
+    }
+}
